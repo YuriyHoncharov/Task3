fd986d3b680e7142c288d7cfe3decf970e4a2325
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-03-31 00:15:21 +0200

    Task3 HardCode
    Working well with 78945/4 , but still not working if divisor is more
    than one digit.

--- task3/src/main/java/ua/com/foxminded/yuriy/task/IntegerDivison.java
+++ task3/src/main/java/ua/com/foxminded/yuriy/task/IntegerDivison.java
@@ -4,152 +4,94 @@ public class IntegerDivison {
 
 	public static void LongDivision(int divident, int divisor) {
 
+		// Basic Variables
 		int quotient = divident / divisor;
-
 		String numDivident = Integer.toString(divident);
 		String numDivisor = Integer.toString(divisor);
+
+		// Temporary Variables
+
+		String tmpStringQuotient = Integer.toString(quotient);
+		String tmpStringDivident = Integer.toString(divident);
+		String tmpStringDivisor = Integer.toString(divisor);
+
+		// Creating int [] array of variables above
+
+		int[] quotientArray = new int[tmpStringQuotient.length()];
+		int[] dividentArray = new int[tmpStringDivident.length()];
+		int[] divisorArray = new int[tmpStringDivisor.length()];
+
+		for (int i = 0; i < tmpStringQuotient.length(); i++) {
+			quotientArray[i] = Character.getNumericValue(tmpStringQuotient.charAt(i));
+		}
+		for (int i = 0; i < tmpStringDivident.length(); i++) {
+			dividentArray[i] = Character.getNumericValue(tmpStringDivident.charAt(i));
+		}
+		for (int i = 0; i < tmpStringDivisor.length(); i++) {
+			divisorArray[i] = Character.getNumericValue(tmpStringDivisor.charAt(i));
+		}
+
+		// StringBuilders for printing result
+
 		StringBuilder divisionResult = new StringBuilder();
-
-		char[] dividentArray = numDivident.toCharArray();
-		char[] divisorArray = numDivisor.toCharArray();
-
-		int[] numericDividentArray = new int[dividentArray.length];
-		for (int i = 0; i < dividentArray.length; i++) {
-			numericDividentArray[i] = Character.getNumericValue(dividentArray[i]);
-		}
-
-		int[] numericDivisorArray = new int[dividentArray.length];
-
-		for (int i = 0; i < divisorArray.length; i++) {
-			numericDivisorArray[i] = Character.getNumericValue(divisorArray[i]);
-		}
-
-		int divisorLength = divisorArray.length;
-		int dividentLength = dividentArray.length;
-
-		StringBuilder leftSpace = new StringBuilder();
+		StringBuilder equalSign = new StringBuilder();
 		StringBuilder regulatorySpace = new StringBuilder();
 		StringBuilder regulatoryDash = new StringBuilder();
+		StringBuilder variableSpace = new StringBuilder();
 
-		for (int i = 0; i < dividentLength - divisorArray.length; i++) {
+		for (int i = 0; i < dividentArray.length - divisorArray.length; i++) {
 			regulatorySpace.append(" ");
 			regulatoryDash.append("-");
 		}
 
 		for (int i = 0; i < divisorArray.length; i++) {
-			leftSpace.append(" ");
+			equalSign.append("-");
 		}
 
-		System.out.println("_" + divident + " | " + divisor);
-		System.out.println(" " + divisor + regulatorySpace + " | " + regulatoryDash);
-		System.out.println(leftSpace + "-" + regulatorySpace + " | " + quotient);
+		// Body of the Integer Division
 
-		StringBuilder tmpDividentToStartIteration = new StringBuilder();
-		for (int i = 0; i < divisorLength; i++) {
-			tmpDividentToStartIteration.append(dividentArray[i]);
-		}
-		int tmpDivident = Integer.parseInt(tmpDividentToStartIteration.toString());
+		divisionResult.append("_" + divident + " | " + divisor + '\n');
+		divisionResult.append(" " + (divisor * quotientArray[0]) + regulatorySpace + " | " + regulatoryDash + '\n');
+		divisionResult.append(" " + equalSign + regulatorySpace + " | " + quotient + '\n');
 
-		int tmpRemainder = 0;
-		int tmpResult = 0;
-		int numberToPrint = 0;
-while (dividentLength > 0) {
-	
-		while (tmpDivident >= divisor) {
-			tmpDivident = tmpDivident - divisor;
-			tmpResult = tmpDivident;
-			tmpRemainder++;
-			numberToPrint = (tmpDivident / divisor) * divisor;
+		// First Iteration to Start
+
+		String tmpNumberToSubtract = "";
+
+		for (int i = 0; i < divisorArray.length; i++) {
+			tmpNumberToSubtract += dividentArray[i];
 		}
 
-		divisionResult.append(tmpRemainder);
+		int initialNumberToSubtract = Integer.parseInt(tmpNumberToSubtract);
+		int resultBeforeStartIteration = initialNumberToSubtract - divisor;
+		int dividentPosition = tmpNumberToSubtract.length();
+		int divisorPosition = 1;
 
-		// Most probably i'll start there my while iteration for full code.
-		
-		StringBuilder subtractResultBuilder = new StringBuilder();
-		subtractResultBuilder.append(tmpResult);
-		subtractResultBuilder.append(numericDividentArray[divisorLength]);
+		// Main Iteration
 
-		
-		int tmpIterationResult = 0;
-		
-		
-
-			
-			int tmpIterationDivident = Integer.parseInt(subtractResultBuilder.toString());
-			numberToPrint = ( tmpIterationDivident / divisor ) * divisor;
-			System.out.println("------------");
-			System.out.println("_" + tmpIterationDivident);
-			System.out.println(" " + numberToPrint);
-			
-
-			while (tmpIterationDivident >= divisor) {
-				tmpIterationDivident = tmpIterationDivident - divisor;
-				tmpIterationResult = tmpIterationDivident;
-				tmpRemainder++;
-				numberToPrint = (tmpIterationDivident / divisor) * divisor;
-
+		for (int i = 1; i < dividentArray.length; i++) {
+			String tmpCreateDivident = (Integer.toString(resultBeforeStartIteration) + dividentArray[dividentPosition]);
+			int tmpDivident = Integer.parseInt(tmpCreateDivident);
+			divisionResult.append("_");
+			divisionResult.append(tmpDivident);
+			int tmpDivisor = (divisor * quotientArray[dividentPosition]);
+			variableSpace.append(" ");
+			divisionResult.append('\n');
+			divisionResult.append(variableSpace);
+			divisionResult.append(tmpDivisor);
+			divisionResult.append('\n').append(variableSpace).append(equalSign).append(equalSign);
+			divisionResult.append('\n');
+			divisionResult.append(variableSpace);
+			//divisionResult.append(equalSign);
+			resultBeforeStartIteration = tmpDivident - tmpDivisor;
+			dividentPosition++;
+			divisorPosition++;
+			if (dividentPosition == dividentArray.length) {
+				divisionResult.append(resultBeforeStartIteration);
 			}
-
-			divisionResult.append(tmpRemainder);
-			dividentLength--;
-			divisorLength++;
-			
 			
 		}
-//			StringBuilder nextIterationResult = new StringBuilder();
-//			nextIterationResult.append(tmpIterationResult);
-//			nextIterationResult.append(numericDividentArray[divisorLength]);
-//			System.out.println("_" + nextIterationResult);
-//
-//			divisorLength++;
-//
-//			int iterationResult = 0;
-//			int nextIterationDivident = Integer.parseInt(nextIterationResult.toString());
-//
-//			while (nextIterationDivident >= divisor) {
-//				nextIterationDivident = nextIterationDivident - divisor;
-//				iterationResult = nextIterationDivident;
-//				tmpRemainder++;
-//			}
-//
-//			divisionResult.append(tmpRemainder);
-//
-//			dividentLength = dividentLength - 2;
-//
-//		}
 
-//		int dividentLength = dividentArray.length;
-//		StringBuilder leftSpace = new StringBuilder();
-//		StringBuilder regulatorySpace = new StringBuilder();
-//		StringBuilder regulatoryDash = new StringBuilder();
-//
-//		for (int i = 0; i < dividentLength - divisorArray.length; i++) {
-//			regulatorySpace.append(" ");
-//			regulatoryDash.append("-");
-//		}
-//
-//		for (int i = 0; i < divisorArray.length; i++) {
-//			leftSpace.append(" ");
-//		}
-//		System.out.println("_" + divident + " | " + divisor);
-//		System.out.println(" " + divisor + regulatorySpace + " | " + regulatoryDash);
-//		System.out.println(leftSpace + "-" + regulatorySpace + " | " + quotient);
-//
-//		for (int i = 0; i < dividentLength - 1; i++) {
-//
-//			int tmpDividentSandBox = numericDividentArray[i];
-//			int tmpDifference = tmpDivident - divisor;
-//			int tmpSecondDividentNumber = numericDividentArray[i + 1];
-//
-//			String tmpStringToMerge = String.valueOf(tmpDifference) + String.valueOf(tmpSecondDividentNumber);
-//			int dividentNumber = Integer.parseInt(tmpStringToMerge);
-//			System.out.println("_" + dividentNumber);
-//			int nextDivisor = ((int) Math.floor(dividentNumber / 4)) * 4;
-//			System.out.println(leftSpace + "--");
-//
-//		}
-
+		System.out.println(divisionResult.toString());
 	}
-
 }
