778238ef0a092684ea9bddaec0bb11b2a394b98d
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-04-09 15:04:57 +0200

    Modified test
    Finished tests: 4/4 +

--- task3/src/test/java/ua/com/foxminded/yuriy/task/IntegerDivisionTest.java
+++ task3/src/test/java/ua/com/foxminded/yuriy/task/IntegerDivisionTest.java
@@ -1,57 +1,57 @@
 package ua.com.foxminded.yuriy.task;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
 
 class IntegerDivisionTest {
-
-	IntegerDivision test = new IntegerDivision();
-	IntegerDivision testTwo = new IntegerDivision();
-	IntegerDivision testThree = new IntegerDivision();
 	
-	
-	// First Case Data
-	
-	int dividend_caseOne = 888;
-	int divisor_caseOne = 888;
-	String result_caseOne = test.LongDivision(dividend_caseOne, divisor_caseOne);
-	String expectedResult_caseOne = "_888 | 888" + "\n"
-								         + " 888 | -" + "\n"
-								         + " --- | 1" + "\n";
-	
-	// Second Case Data
-	
-	int dividend_caseTwo = 66;
-	int divisor_caseTwo = 1;
-	String result_caseTwo = testTwo.LongDivision(dividend_caseTwo, divisor_caseTwo);
-	String expectedResult_caseTwo = "_66 | 1" + "\n"
-			 							   + " 6  | --" + "\n"
-			                        + " -  | 66"  + "\n"
-			                        + " _6" + "\n"
-			                        + "  6" + "\n"
-			                        + "  -" + "\n"
-			                        + "  0";
-	// Third Case Data
-	
-//	int dividend_caseThree = 0;
-//	int divisor_caseThree = 0;
-//	String result_caseThree = testThree.LongDivision(dividend_caseThree, divisor_caseThree);
+	public static final IntegerDivision test = new IntegerDivision();
 
 	@Test
 	void LongDivision_DivisorEqualToDividend_RightPrintResult() {
+		int dividendEqualDivisor = 888;
+		IntegerDivision test = new IntegerDivision();
+		String result_caseOne = test.LongDivision(dividendEqualDivisor, dividendEqualDivisor);
+		String expectedResult_caseOne = "_888 | 888" + "\n"
+								         	+ " 888 | -" + "\n"
+								         	+ " --- | 1" + "\n";
 		assertEquals(expectedResult_caseOne, result_caseOne);
-		
+
 	}
+
 	@Test
 	void LongDivision_DivisorEqualToOne_RightPrintResult() {
+		int dividend = 66;
+		int divisor = 1;
+		IntegerDivision test = new IntegerDivision();
+		String result_caseTwo = test.LongDivision(dividend, divisor);
+		String expectedResult_caseTwo = "_66 | 1" + "\n"
+			 							   	+ " 6  | --" + "\n"
+			 							   	+ " -  | 66"  + "\n"
+			 							   	+ " _6" + "\n"
+			 							   	+ "  6" + "\n"
+			 							   	+ "  -" + "\n"
+			 							   	+ "  0";
 		assertEquals(expectedResult_caseTwo, result_caseTwo);
-		
+
 	}
-	
-//	@Test
-//	void LongDivision_DivisorOrDividendEqualZero_ThrowIllegalException() {
-//		assertThrows(IllegalArgumentException.class, () -> testThree.LongDivision(dividend_caseThree, divisor_caseThree));
-//	}
-	
+
+	@Test
+	void LongDivision_DividendEqualZero_ThrowIllegalException() {
+		IntegerDivision test = new IntegerDivision();
+		int dividend = 0;
+		int divisor = 100500;
+		assertThrows(IllegalArgumentException.class, () -> test.LongDivision(dividend, divisor));
+	}
+	@Test
+	void LongDivision_DivisorEqualZero_ThrowIllegalException() {
+		IntegerDivision test = new IntegerDivision();
+		int dividend = 100500;
+		int divisor = 0;
+		assertThrows(IllegalArgumentException.class, () -> test.LongDivision(dividend, divisor));
+	}
 }
